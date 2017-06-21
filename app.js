c1a168d0c5ff892bbe702172ff5e5e9b970ddbcf
Renan de Souza Antunes <renandesouzaantunes@gmail.com>
2017-06-21 11:08:31 -0300

    Bug Fixes

--- app.js
+++ app.js
@@ -10,151 +10,133 @@ $(document).ready(function(){
 	var membros = 0;
 	var total = 0;
 
-	/*
-	$('#equipe-executora input').on('change', function(evt) {
-	var temp = parseInt($(this).val());
+	$('.btnNext').click(function(){
+		$('.nav-pills > .active').next('li').find('a').trigger('click');
+		$("html, body").animate({ scrollTop: 400 }, 600);
+	});
+	$('.btnPrev').click(function(){
+		$('.nav-pills > .active').prev('li').find('a').trigger('click');
+		$("html, body").animate({ scrollTop: 400 }, 600);
+	});
+	var max_fields      = 10; //maximum input boxes allowed
+	var wrapper;
+	var id;
+	var x = 0; //initlal text box count
 
-	for( i = membros; i < temp + membros ; i++ ){
-	$("#equipe-info").append(
-	"<label>MEMBRO " + (i + 1) + "</label>" +
-	"<div class='form-group'>" +
-	"<label>NOME COMPLETO</label>" +
-	"<input type='text' class='form-control'>" +
-	"</div>" +
-	"<div class='form-group'>" +
-	"<label>CPF</label>" +
-	"<input type='text' class='form-control'>" +
-	"</div>" +
-	"<div class='form-group'>" +
-	"<label>CARGA HORÁRIA SEMANAL</label>" +
-	"<input type='time' class='form-control'>" +
-	"</div>" +
-	"<div class='form-group'>" +
-	"<label>E-MAIL</label>" +
-	"<input type='text' class='form-control'>" +
-	"</div>" +
-	"<div class='form-group'>" +
-	"<label>FUNÇÃO</label>" +
-	"<input type='text' class='form-control'>" +
-	"</div><hr>");
-};
-membros += temp;
-})
-*/
-
-$('.btnNext').click(function(){
-	$('.nav-pills > .active').next('li').find('a').trigger('click');
-	$("html, body").animate({ scrollTop: 400 }, 600);
-});
-$('.btnPrev').click(function(){
-	$('.nav-pills > .active').prev('li').find('a').trigger('click');
-	$("html, body").animate({ scrollTop: 400 }, 600);
-});
-
-
-	$('#prox').click(function () {
-		var sum = 0;
-
-		$('.membros').each(function() {
-			sum += Number($(this).val());
-			membros += Number($(this).val());
-		});
-
-		var aux = total;
-		console.log(sum);
-		console.log(membros);
-		console.log(aux);
-
-		if(sum > 50) {
-			sum = 50;
-			alert("O limite de membros é 50.");
+	$(".adc").click(function(e){ //on add input button click
+		e.preventDefault();
+		id = $(this).attr('id'); // $(this) refers to button that was clicked
+		switch (id) {
+			case '1':
+			wrapper = "#docentes";
+			break;
+			case '2':
+			wrapper = "#tecnicos";
+			break;
+			case '3':
+			wrapper = "#voluntarios";
+			break;
+			case '4':
+			wrapper = "#utfpr";
+			break;
+			case '5':
+			wrapper = "#outras";
+			break;
+			case '6':
+			wrapper = "#prorec";
+			break;
+			case '7':
+			wrapper = "#fapibex";
+			break;
+			case '8':
+			wrapper = "#fapibis";
+			break;
+			case '9':
+			wrapper = "#pet";
+			break;
+			default:
+			break;
 		}
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append("<div><div class='form-group'><label>NOME COMPLETO</label><input type='text' class='form-control'></div><div class='form-group'><label>CPF</label><input type='text' class='form-control'></div><div class='form-group'><label>CARGA HORÁRIA SEMANAL</label><input type='time' class='form-control'></div><div class='form-group'><label>E-MAIL</label><input type='text' class='form-control'></div><div class='form-group'><label>FUNÇÃO</label><input type='text' class='form-control'></div><button class='btn btn-danger bnt-sm remove_field'>Remover</button><hr></div>"); //add input box
+		}
+		else{
+			alert("Limite de membros atingido!");
+		}
+	});
 
-		for( i = aux; i < sum; i++ ){
-			$("#equipe-info").append(
-				"<label>MEMBRO " + (i + 1) + "</label>" +
-				"<div class='form-group'>" +
-				"<label>NOME COMPLETO</label>" +
-				"<input type='text' class='form-control'>" +
-				"</div>" +
-				"<div class='form-group'>" +
-				"<label>CPF</label>" +
-				"<input type='text' class='form-control'>" +
-				"</div>" +
-				"<div class='form-group'>" +
-				"<label>CARGA HORÁRIA SEMANAL</label>" +
-				"<input type='time' class='form-control'>" +
-				"</div>" +
-				"<div class='form-group'>" +
-				"<label>E-MAIL</label>" +
-				"<input type='text' class='form-control'>" +
-				"</div>" +
-				"<div class='form-group'>" +
-				"<label>FUNÇÃO</label>" +
-				"<input type='text' class='form-control'>" +
-				"</div><hr>");
-				total++;
-			};
-		});
+	$(document).on('click', '.remove_field', function(e){ //user click on remove text
+		e.preventDefault();
+		$(this).parent('div').remove();
+		x--;
+	});
+
+	$("#inputPassword").keyup(function(event){
+		console.log("OLA");
+		if(event.keyCode == 13){
+			$("#login-ok").click();
+		}
+	});
 });
 
 (function ($) {
-    $(function () {
+	$(function () {
 
-        var addFormGroup = function (event) {
-            event.preventDefault();
+		var addFormGroup = function (event) {
+			event.preventDefault();
 
-            var $formGroup = $(this).closest('.form-group');
-            var $multipleFormGroup = $formGroup.closest('.multiple-form-group');
-            var $formGroupClone = $formGroup.clone();
+			var $formGroup = $(this).closest('.form-group');
+			var $multipleFormGroup = $formGroup.closest('.multiple-form-group');
+			var $formGroupClone = $formGroup.clone();
 
-            $(this)
-                .toggleClass('btn-success btn-add btn-danger btn-remove')
-                .html('–');
+			$(this)
+			.toggleClass('btn-success btn-add btn-danger btn-remove')
+			.html('–');
 
-            $formGroupClone.find('input').val('');
-            $formGroupClone.find('.concept').text('Phone');
-            $formGroupClone.insertAfter($formGroup);
+			$formGroupClone.find('input').val('');
+			$formGroupClone.find('.concept').text('Phone');
+			$formGroupClone.insertAfter($formGroup);
 
-            var $lastFormGroupLast = $multipleFormGroup.find('.form-group:last');
-            if ($multipleFormGroup.data('max') <= countFormGroup($multipleFormGroup)) {
-                $lastFormGroupLast.find('.btn-add').attr('disabled', true);
-            }
-        };
+			var $lastFormGroupLast = $multipleFormGroup.find('.form-group:last');
+			if ($multipleFormGroup.data('max') <= countFormGroup($multipleFormGroup)) {
+				$lastFormGroupLast.find('.btn-add').attr('disabled', true);
+			}
+		};
 
-        var removeFormGroup = function (event) {
-            event.preventDefault();
+		var removeFormGroup = function (event) {
+			event.preventDefault();
 
-            var $formGroup = $(this).closest('.form-group');
-            var $multipleFormGroup = $formGroup.closest('.multiple-form-group');
+			var $formGroup = $(this).closest('.form-group');
+			var $multipleFormGroup = $formGroup.closest('.multiple-form-group');
 
-            var $lastFormGroupLast = $multipleFormGroup.find('.form-group:last');
-            if ($multipleFormGroup.data('max') >= countFormGroup($multipleFormGroup)) {
-                $lastFormGroupLast.find('.btn-add').attr('disabled', false);
-            }
+			var $lastFormGroupLast = $multipleFormGroup.find('.form-group:last');
+			if ($multipleFormGroup.data('max') >= countFormGroup($multipleFormGroup)) {
+				$lastFormGroupLast.find('.btn-add').attr('disabled', false);
+			}
 
-            $formGroup.remove();
-        };
+			$formGroup.remove();
+		};
 
-        var selectFormGroup = function (event) {
-            event.preventDefault();
+		var selectFormGroup = function (event) {
+			event.preventDefault();
 
-            var $selectGroup = $(this).closest('.input-group-select');
-            var param = $(this).attr("href").replace("#","");
-            var concept = $(this).text();
+			var $selectGroup = $(this).closest('.input-group-select');
+			var param = $(this).attr("href").replace("#","");
+			var concept = $(this).text();
 
-            $selectGroup.find('.concept').text(concept);
-            $selectGroup.find('.input-group-select-val').val(param);
+			$selectGroup.find('.concept').text(concept);
+			$selectGroup.find('.input-group-select-val').val(param);
 
-        }
+		}
 
-        var countFormGroup = function ($form) {
-            return $form.find('.form-group').length;
-        };
+		var countFormGroup = function ($form) {
+			return $form.find('.form-group').length;
+		};
 
-        $(document).on('click', '.btn-add', addFormGroup);
-        $(document).on('click', '.btn-remove', removeFormGroup);
-        $(document).on('click', '.dropdown-menu a', selectFormGroup);
+		$(document).on('click', '.btn-add', addFormGroup);
+		$(document).on('click', '.btn-remove', removeFormGroup);
+		$(document).on('click', '.dropdown-menu a', selectFormGroup);
 
-    });
+	});
 })(jQuery);
